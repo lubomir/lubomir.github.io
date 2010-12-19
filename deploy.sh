a823524601d6f7d63e33813dd79a237ad1085df7
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2010-12-19 15:37:48 +0100

    Added script for deploying to aisa

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+echo "*** Compiling Hakyll ***"
+ghc --make hakyll.hs
+
+echo "*** Building site ***"
+./hakyll rebuild
+
+echo "*** Syncing to server ***"
+rsync -vrp --checksum _site/ xsedlar3@aisa.fi.muni.cz:/home/xsedlar3/public_html
